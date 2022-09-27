59111232125e7aa08ddb3b10af01ac86ff701234
Neo Pan <panlw@kinco.cn>
2022-09-27 17:29:18 +0800

    initial commit

---
+++ wasm-lib/src/lib.rs
@@ -0,0 +1,17 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
